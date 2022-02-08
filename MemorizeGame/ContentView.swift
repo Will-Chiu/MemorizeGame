c6f357bf6a7fbaf52b60423243ddd632c70cde98
Will-Chiu <chiukayu@gmail.com>
2022-02-08 23:50:03 +0000

    Initial Commit

---
+++ MemorizeGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MemorizeGame
+//
+//  Created by Entangled Mind on 8/2/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Entangled Mind on 8/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
